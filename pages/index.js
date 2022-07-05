4716e5d12962ffd56c81a9f428d3ced60669aebf
sandorfalot <sandorfalot@gmail.com>
2022-07-05 04:22:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function MonkeyPox({ data }) {
     return <p>{JSON.stringify(data)}</p>
 }
 
-export async function getServerSideProps() {
+export const  getServerSideProps  = async () => {
     const headers = new Headers();
     headers.append('X-RapidAPI-Key', '6fa680398fmshadf4be5c3caa2bep145905jsn9d73ba413785',
         'X-RapidAPI-Host', 'monkeypox-poxvirus-statistics.p.rapidapi.com');
@@ -18,4 +18,39 @@ export async function getServerSideProps() {
     const data = await res.json()
     return { props: { data } }
 }
-export default MonkeyPox
+
+//route
+export default function Home({ data }) {
+    return (
+        <div>
+            <main>
+                <center><h1>Monkeypox Tracker</h1></center>
+                 <table>
+                        <tr><td><h4>Country</h4></td>
+                        <td><h4>Confirmed</h4></td>
+                        <td><h4>Suspected</h4></td>
+                            <td><h4>Total</h4></td>
+                        <td><h4>Deaths</h4></td>
+                        </tr>
+                {data.map(data => (
+                   <tr>
+                            <td>{data.country}</td>
+                            <td>{data.confirmed}</td>
+                            <td>{data.suspected}</td>
+                            <td>{data.total}</td>
+                            <td>{data.deaths}</td>
+                        </tr>
+                  
+                ))}
+                      </table>
+            </main>
+            <footer>
+                <center>Data from <a href="https://rapidapi.com/gAlexander10/api/monkeypox-global-statistics/">
+                Monkeypox Global Statistics API</a></center>
+            </footer>
+        </div>
+        
+    )
+}
+    
+
